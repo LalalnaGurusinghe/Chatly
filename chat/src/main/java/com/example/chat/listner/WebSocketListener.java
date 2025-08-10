72bf73e48a79e2d5c50aef2da9dd98e95794d2cc
LalanaGurusinghe <lalanagurusinghe@gmail.com>
2025-08-10 14:25:53 +0530

    Rename and implement WebSocketListener class
    Replaces the incorrectly named WebSocketListner with WebSocketListener and adds logic for handling websocket connect and disconnect events, updating user status and broadcasting leave messages.

---
+++ chat/src/main/java/com/example/chat/listner/WebSocketListener.java
@@ -0,0 +1,43 @@
+package com.example.chat.listner;
+
+import com.example.chat.model.ChatMessage;
+import com.example.chat.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+public class WebSocketListener {
+    private final UserService userService;
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketListener.class);
+
+    public WebSocketListener(UserService userService) {
+        this.userService = userService;
+    }
+
+    @EventListener
+    public void handleWebsocketConnectListener(SessionConnectedEvent event){
+        logger.info("Connected to websocket");
+    }
+
+    public void handleWebsocketDisconnectListener(SessionDisconnectEvent event){
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = headerAccessor.getSessionAttributes().get("username").toString();
+
+        System.out.println("User disconnected: " + username + " with Session ID: " + headerAccessor.getSessionId());
+        userService.setUserOnlineStatus(username,false);
+        ChatMessage chatMessage = new ChatMessage();
+        chatMessage.setMessageType(ChatMessage.MessageType.LEAVE);
+        chatMessage.setSender(username);
+        messagingTemplate.convertAndSend("/topic/public",chatMessage);
+    }
+}
--- chat/src/main/java/com/example/chat/listner/WebSocketListner.java
+++
@@ -1,4 +0,0 @@
-package com.example.chat.listner;
-
-public class WebSocketListner {
-}
